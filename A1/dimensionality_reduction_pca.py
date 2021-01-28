e9103f477b86b69fa28aadc731e50d84196765c7
Kevin Tayah <ktayah@yahoo.com>
2021-01-28 11:03:33 -0500

    Added final changes to problem 2

--- A1/dimensionality_reduction_pca.py
+++ A1/dimensionality_reduction_pca.py
@@ -22,21 +22,21 @@ def load_data():
 
 def pca(data):
     standardize_data = (data - np.mean(data, axis=0)) / np.std(data, axis=0, ddof=1)
-    covariance_matrix = np.cov(standardize_data)
+    covariance_matrix = np.cov(standardize_data.T, ddof=1)
     eigen_values, eigen_vectors = np.linalg.eig(covariance_matrix)
 
-    pcaValues = []
-    for index, standardize_row in enumerate(standardize_data):
-        print(index, standardize_data)
-        pcaValues.append(np.dot(standardize_row, eigen_vectors[index]))
+    max_eigen_value_indices = (-eigen_values).argsort()[:2] # Finds indices of 2 highest eigen_values
+    
+    pca1 = np.dot(standardize_data, eigen_vectors[:,max_eigen_value_indices[0]])
+    pca2 = np.dot(standardize_data, eigen_vectors[:,max_eigen_value_indices[1]])
 
-    return pcaValues
+    return pca1, pca2
 
 def main():
     images = load_data()
     two_d_reduction = pca(images)
 
-    plt.plot(two_d_reduction)
+    plt.plot(two_d_reduction[0], two_d_reduction[1], 'ro')
     plt.show()
 
 if __name__ == "__main__":
