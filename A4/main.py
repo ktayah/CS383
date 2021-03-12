134f3248d5e9c851a95afdd9b031008c917e9338
Kevin Tayah <ktayah@yahoo.com>
2021-03-12 00:05:32 -0500

    Added accuracy, precision, recall, and f-measure

--- A4/main.py
+++ A4/main.py
@@ -22,11 +22,27 @@ def load_data(file):
 def preprocess_data(data):
     return standardize(data[:,:-1]), data[:,-1]
 
+def precision(y_true, y_pred):
+    true_positives = np.sum(y_true == y_pred)
+    f_p_bools = [y_true[i] != y_p and y_p == 1 for i, y_p in enumerate(y_pred)]
+    false_positives = np.sum(f_p_bools)
+    return true_positives / true_positives + false_positives
+
+def recall(y_true, y_pred):
+    true_positives = np.sum(y_true == y_pred)
+    f_n_bools = [y_true[i] != y_p and y_p == 0 for i, y_p in enumerate(y_pred)]
+    false_negatives = np.sum(f_n_bools)
+    return true_positives / true_positives + false_negatives
+
 def accuracy(y_true, y_pred):
-    accuracy = np.sum(y_true == y_pred) / len(y_true)
-    return accuracy
+    return np.sum(y_true == y_pred) / len(y_true)
 
-def naive_bayes():
+def f_measure(y_true, y_pred):
+    p = precision(y_true, y_pred)
+    r = recall(y_true, y_pred)
+    return (2 * p  * r) / (p + r)
+
+def main():
     file = 'spambase.data'
     train, validation = load_data(file)
 
@@ -34,32 +50,25 @@ def naive_bayes():
     train_x, train_y = preprocess_data(train)
     validation_x, validation_y = preprocess_data(validation)
 
-    # Run Naive Bayes fit and predict
-    nb = NaiveBayes()
-    nb.fit(train_x, train_y)
-    predictions = nb.predict(validation_x)
+    predictions = None
+    if sys.argv[1] == 'nb':
+        # Run Naive Bayes fit and predict
+        nb = NaiveBayes()
+        nb.fit(train_x, train_y)
+        predictions = nb.predict(validation_x)
+    else:
+        # Run Logistic Regression fit and predict
+        lr = LogisticRegression()
+        lr.fit(train_x, train_y)
+        predictions = lr.predict(validation_x)
 
-    print('Accuracy:', accuracy(validation_y, predictions))
-
-def logistic_regression():
-    file = 'spambase.data'
-    train, validation = load_data(file)
-
-    # Standardize features and extract classes
-    train_x, train_y = preprocess_data(train)
-    validation_x, validation_y = preprocess_data(validation)
-
-    # Run Logistic Regression fit and predict
-    lr = LogisticRegression()
-    lr.fit(train_x, train_y)
-    predictions = lr.predict(validation_x)
-
+    print('Precision:', precision(validation_y, predictions))
+    print('Recall:', recall(validation_y, predictions))
+    print('f-measure:', f_measure(validation_y, predictions))
     print('Accuracy:', accuracy(validation_y, predictions))
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2 and sys.argv[1] == 'nb':
-        naive_bayes()
-    elif len(sys.argv) == 2 and sys.argv[1] == 'lr':
-        logistic_regression()
+    if len(sys.argv) == 2:
+        main()
     else:
         print('Incorrect usage. Execute "python main.py nb" for the naive bayes algorithm or "python main.py lr" for logistic regression algorithm')
